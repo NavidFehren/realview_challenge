ad0dcc7c54657336dd5623276f96f9922f99046d
NavidF <nfehren@q-leap.de>
2025-05-16 18:31:05 +0200

    Add client-side filtering for the results

--- lib/data/remote/authors_remote_data_source_impl.dart
+++ lib/data/remote/authors_remote_data_source_impl.dart
@@ -19,7 +19,15 @@ class AuthorsRemoteDataSourceImpl implements AuthorsRemoteDataSource {
 
     if (response.statusCode == 200) {
       final List<dynamic> docs = response.data['docs'];
-      return docs.map((doc) => AuthorModel.fromJson(doc)).toList();
+      final List<AuthorModel> allAuthors = docs.map((doc) => AuthorModel.fromJson(doc)).toList();
+      
+      // Filter results to only include authors whose names contain the exact query string
+      if (query.isNotEmpty) {
+        return allAuthors.where((author) => 
+          author.name.toLowerCase().contains(query.toLowerCase())).toList();
+      }
+      
+      return allAuthors;
     } else {
       throw Exception('Failed to search authors: ${response.statusCode}');
     }
